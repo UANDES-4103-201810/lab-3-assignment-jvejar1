bb2adce1255557a63641e6bd885e342f87a3fed5
e440 <jvejar1@miuandes.cl>
2018-03-27 15:30:34 -0300

    3.c

--- ticketpro/app/models/user.rb
+++ ticketpro/app/models/user.rb
@@ -3,13 +3,17 @@ class User < ApplicationRecord
   has_many :tickets, through: :ticket_users
 
   def most_expensive_ticket_bought
-    return self.tickets.max_by(&:price)
+    return self.tickets.max_by(&:price).price
   end
 
   def most_expensive_ticket_bought_between(start_date,end_date)
 
     return self.tickets.where(:date_bought=>start_date..end_date).max_by(&:price)
 
+  end
+
+  def last_event
+    return self.ticket_users.max_by(&:date_bought).event
   end
 end
 
